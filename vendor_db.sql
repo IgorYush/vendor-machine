2c325790e7d3f174d45e4ef3e21bcb21dfe3e318
ihor.yushchak <ihor.yushchak@mdoc.one>
2022-09-27 23:51:06 +0100

    update users_roles for admin

--- vendor_db.sql
+++ vendor_db.sql
@@ -46,9 +46,7 @@ INSERT into vendor.roles VALUES ('ebb29ccb-7238-4833-833e-c3963d2d0e5f', 'ROLE_A
 INSERT into vendor.roles VALUES ('8af7afc4-df95-4a28-91ec-63940ae90077', 'ROLE_SELLER');
 
 INSERT into vendor.roles VALUES ('8bb71819-40ff-41b5-8052-0517ae497965', 'ROLE_BUYER');
-
-
 INSERT into vendor.wallet  VALUES ('29c3e8f5-87aa-4622-9dc0-b35178b7ce1a', 0);
-
-
 INSERT into vendor.users VALUES ('1f1c5edf-6b03-4376-bf04-beadbb9960b8', 'admin', '$2a$10$CnP5l7XUT4QwBNd2shJ0refV.hN69kmdAUDrSo.6sf.ZjgWiUdaH6', '2022-09-27 00:18:56.808', false, '29c3e8f5-87aa-4622-9dc0-b35178b7ce1a');
+insert into vendor.users_roles values ('1f1c5edf-6b03-4376-bf04-beadbb9960b8','8dd8c73a-06bf-4e1b-9959-8b08006d54ec');
+insert into vendor.users_roles values ('1f1c5edf-6b03-4376-bf04-beadbb9960b8','ebb29ccb-7238-4833-833e-c3963d2d0e5f');
